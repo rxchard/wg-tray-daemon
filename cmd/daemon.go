76fa9590d85678f8ac120cc77acfc83a3723cd2d
rxchard <whidex@riseup.net>
2021-04-15 12:55:15 +0200

    formatting

--- cmd/daemon.go
+++ cmd/daemon.go
@@ -1,30 +1,30 @@
 package cmd
 
 import (
-    "context"
-    "github.com/rxchard/wg-tray-daemon/internal/server"
-    "log"
-    "os"
-    "os/signal"
-    "syscall"
+	"context"
+	"github.com/rxchard/wg-tray-daemon/internal/server"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 var rootx context.Context
 
 func Execute() {
-    var cancel context.CancelFunc
-    rootx, cancel = context.WithCancel(context.Background())
-    defer cancel()
+	var cancel context.CancelFunc
+	rootx, cancel = context.WithCancel(context.Background())
+	defer cancel()
 
-    channel := make(chan os.Signal)
-    signal.Notify(channel, os.Interrupt, syscall.SIGTERM)
+	channel := make(chan os.Signal)
+	signal.Notify(channel, os.Interrupt, syscall.SIGTERM)
 
-    go func() {
-      <-channel
-      cancel()
-    }()
+	go func() {
+		<-channel
+		cancel()
+	}()
 
-    if err := server.Execute(rootx); err != nil {
-        log.Fatal(err)
-    }
+	if err := server.Execute(rootx); err != nil {
+		log.Fatal(err)
+	}
 }
